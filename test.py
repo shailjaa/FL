5e1990b8458f6cb7b7c0986746908e2adfafee2b
shailjaa <62165403+shailjaa@users.noreply.github.com>
2022-02-03 17:07:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -16,63 +16,60 @@ app = Flask(__name__)
 s3_client = boto3.client('s3', 
                       aws_access_key_id='AKIATOXCR6TP47PYZPW7', 
                       aws_secret_access_key='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')
-session = boto3.Session(aws_access_key_id='AKIATOXCR6TP47PYZPW7', 
-                      aws_secret_access_key='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', 
-                      )
 base_directory = "./dataset/process_data/"
 
 def chunking(request_data):
-    try :
-        s3 = session.resource('s3')
-        my_bucket = request_data['bucket_name']
-        total_jobs = ast.literal_eval(request_data['total_jobs'])
-        s3_client.download_file(my_bucket, request_data['dataset'], request_data['dataset'].split("/")[-1])
-        with zipfile.ZipFile(request_data['dataset'].split("/")[-1], 'r') as zip_ref:
-            zip_ref.extractall("./dataset/")
+    my_bucket = request_data['bucket_name']
+    total_jobs = ast.literal_eval(request_data['total_jobs'])
+    s3_client.download_file(my_bucket, request_data['dataset'], request_data['dataset'].split("/")[-1])
+    with zipfile.ZipFile(request_data['dataset'].split("/")[-1], 'r') as zip_ref:
+        zip_ref.extractall("./dataset/")
 
-        data = torchvision.datasets.ImageFolder(root = "./dataset/" + request_data['dataset'].split(".zip")[-1] + "/",  transform=torchvision.transforms.Compose(
-                [torchvision.transforms.Resize([224, 224]),
-                    torchvision.transforms.ToTensor(),
-                    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225])]))
-        train_loader = torch.utils.data.DataLoader(data,batch_size=2, shuffle=True)
+    print("./dataset/" + request_data['dataset'].split(".")[0] + "/")
+    data = torchvision.datasets.ImageFolder(root = "./dataset/" + request_data['dataset'].split(".")[0] + "/",  transform=torchvision.transforms.Compose(
+            [torchvision.transforms.Resize([224, 224]),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])]))
+    train_loader = torch.utils.data.DataLoader(data,batch_size=2, shuffle=True)
 
-        examples = enumerate(train_loader)
-        print(len(train_loader))
-        
-        data_batch_list = []
-        
-        os.makedirs(base_directory + "data/")
-        os.makedirs(base_directory + "target/")
-        os.makedirs(base_directory + "Zip_data/")
+    examples = enumerate(train_loader)
+    print(len(train_loader))
+    
+    data_batch_list = []
+    
+    os.makedirs(base_directory + "data/")
+    os.makedirs(base_directory + "target/")
+    os.makedirs(base_directory + "Zip_data/")
 
-        for i in range(len(train_loader)):
-            batch_idx, (data, target) = next(examples)
-            data_batch_list.append(batch_idx)
-            torch.save(data, base_directory + "data/" + str(batch_idx)+".pt")
-            torch.save(target, base_directory + "target/"+str(batch_idx)+".pt")
+    for i in range(len(train_loader)):
+        batch_idx, (data, target) = next(examples)
+        data_batch_list.append(batch_idx)
+        torch.save(data, base_directory + "data/" + str(batch_idx)+".pt")
+        torch.save(target, base_directory + "target/"+str(batch_idx)+".pt")
 
-        count = 0
-        if total_jobs < 600:
-            data_length=math.ceil(len(train_loader)/total_jobs)
-            for i in range(int(total_jobs)):
-                os.makedirs(base_directory + str(i) + "/data/")
-                os.makedirs(base_directory + str(i) + "/target/")
-                for j in range(int(data_length)):
-                    print("COUNT", count)
-                    if count == len(train_loader):
-                        pass
-                    else : 
-                        random_pt = random.choice(data_batch_list)
-                        data_batch_list.remove(random_pt)
-                        shutil.copy2(base_directory + "data/" + str(random_pt)+".pt", base_directory + str(i)+"/data/" + str(random_pt) + ".pt")
-                        shutil.copy2(base_directory + "target/" + str(random_pt)+".pt", base_directory + str(i)+"/target/" + str(random_pt) + ".pt")
-                        count = count + 1
+    count = 0
+    if total_jobs < 600:
+        data_length=math.ceil(len(train_loader)/total_jobs)
+        for i in range(int(total_jobs)):
+            os.makedirs(base_directory + str(i) + "/data/")
+            os.makedirs(base_directory + str(i) + "/target/")
+            for j in range(int(data_length)):
+                print("COUNT", count)
+                if count == len(train_loader):
+                    pass
+                else : 
+                    random_pt = random.choice(data_batch_list)
+                    data_batch_list.remove(random_pt)
+                    shutil.copy2(base_directory + "data/" + str(random_pt)+".pt", base_directory + str(i)+"/data/" + str(random_pt) + ".pt")
+                    shutil.copy2(base_directory + "target/" + str(random_pt)+".pt", base_directory + str(i)+"/target/" + str(random_pt) + ".pt")
+                    count = count + 1
 
-                make_archive(base_directory + str(i), base_directory + "Zip_data/" + str(i)+ ".zip")
-                s3_client.upload_file( base_directory + "Zip_data/" + str(i)+ ".zip", request_data['bucket_name'], "data/" + str(i) + ".zip")
-    except Exception as e:
-        print("Error : ", e)
+            make_archive(base_directory + str(i), base_directory + "Zip_data/" + str(i)+ ".zip")
+            s3_client.upload_file( base_directory + "Zip_data/" + str(i)+ ".zip", request_data['bucket_name'], "data/" + str(i) + ".zip")
+    # except Exception as e:
+    #     print("Error : ", e)
+    #     traceback.print_exc
 
 def make_archive(source, destination):
         base = os.path.basename(destination)
@@ -84,7 +81,7 @@ def make_archive(source, destination):
         shutil.move('%s.%s'%(name,format), destination)
 
 @app.route('/chunk_data', methods=['POST'])
-def index():
+def index():    
     try:
         if request.method == "POST":
             
